c6a3c5c1701cc871e4d2cc1f237e117ae0a5fee0
hanidea <hydavid@126.com>
2019-10-20 16:09:16 +0800

    firstdemo command

---
+++ firstdemo/firstdemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace firstdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello .netcore");
+        }
+    }
+}
